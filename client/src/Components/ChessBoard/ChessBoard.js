3f116960db68b4e5f60cee7d32407ed60c1b151b
leoopez <leoocode@gmail.com>
2021-08-01 10:26:51 -0500

    render chessboard

--- client/src/Components/ChessBoard/ChessBoard.js
+++ client/src/Components/ChessBoard/ChessBoard.js
@@ -1,47 +1,73 @@
 import React, { useState } from "react";
 
-import CreateSquare from "./SquaresMain";
+//API to validate chess moves by the user (https://github.com/brozeph/node-chess)
+import chess from "chess";
+
 import Square from "./Square";
 
-export default function ChessBoard() {
-  const [chessBoard, setchessBoard] = useState(CreateSquare);
-  const [pieceOnClick, setPieceOnClick] = useState(null);
-  const [lastPieceOnClick, setLastPieceOnClick] = useState(null);
+// const board = chess.Board();
+// console.log(board);
 
+export default function ChessBoard() {
+  const [chessBoard, setchessBoard] = useState(chess.create());
+  const [firstClick, setFirstCLick] = useState(null);
+
+  // console.log(chessBoard);
   const onSelect = (id) => {
-    console.log("In Construction");
-    // if (!chessBoard[id].piece) return;
-    //   let newSquares = [...chessBoard];
-    //   if (pieceOnClick) {
-    //     newSquares[id].piece = newSquares[pieceOnClick].piece;
-    //     newSquares[id].lastMove = true;
-    //     newSquares[pieceOnClick].lastMove = true;
-    //     newSquares[pieceOnClick].selected = false;
-    //     newSquares[pieceOnClick].piece = null;
-    //     if (lastPieceOnClick) {
-    //       newSquares[lastPieceOnClick[0]].lastMove = false;
-    //       newSquares[lastPieceOnClick[0]].lastMove = false;
-    //     }
-    //     setLastPieceOnClick([id, pieceOnClick]);
-    //     setPieceOnClick(null);
-    //   } else {
-    //     newSquares[id].selected = true;
-    //     setPieceOnClick(id);
-    //   }
-    //   setchessBoard(newSquares);
+    //get Square Object with id
+    const square = chessBoard.game.board.squares[id];
+
+    //If is the start of a move(touch a piece)
+    if (!firstClick) {
+      //find if is a posible valid move
+      const validSquare = chessBoard.validMoves.find(
+        (sqr) => sqr.src === square
+      );
+
+      // set the start of a move
+      if (validSquare) {
+        setFirstCLick(validSquare);
+      }
+    } else {
+      // check if is a valid destination of the selected piece
+      const validSquare = firstClick.squares.find((sqr) => sqr === square);
+
+      if (validSquare) {
+        const move = Object.keys(chessBoard.notatedMoves).find((key, index) => {
+          // console.log(
+          //   chessBoard.notatedMoves[key],
+          //   validSquare,
+          //   firstClick.src
+          // );
+          return (
+            chessBoard.notatedMoves[key].dest === validSquare &&
+            chessBoard.notatedMoves[key].src === firstClick.src
+          );
+        });
+        console.log(move);
+        chessBoard.move(move);
+      }
+      setFirstCLick(null);
+    }
   };
 
-  const squaresInit = chessBoard.map((item) => (
-    <Square
-      key={item.id}
-      id={item.id}
-      row={item.row}
-      col={item.col}
-      piece={item.piece}
-      selected={item.selected}
-      onSelect={onSelect}
-      lastSelected={item.lastMove}
-    />
-  ));
+  const squaresInit = chessBoard.game.board.squares.map((item, index) => {
+    const piece = `${item.piece ? item.piece.side.name : null}-${
+      item.piece ? item.piece.type : null
+    }`;
+
+    return (
+      <Square
+        id={index}
+        key={index}
+        piece={piece}
+        row={item.file}
+        col={item.rank}
+        selected={item === firstClick?.src}
+        onSelect={onSelect}
+      />
+    );
+  });
+
   return <div className="layout-chessboard">{squaresInit}</div>;
 }
--- client/src/Components/ChessBoard/Square.js
+++ client/src/Components/ChessBoard/Square.js
@@ -1,21 +1,29 @@
 import React from "react";
 
+const files = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8,
+};
+
 export default function Square({
   id,
   piece,
   col,
   row,
   selected,
-  lastSelected,
   onSelect = (f) => f,
 }) {
   return (
     <div
-      id={`square-${id}`}
-      style={{ gridColumn: col, gridRow: row }}
-      className={`piece ${piece ? piece : ""} ${selected ? "selected" : ""} ${
-        lastSelected ? "last-selected" : ""
-      }`.trim()}
+      // id={`square-${id}`}
+      style={{ gridColumn: files[row], gridRow: Math.abs(9 - col) }}
+      className={`piece ${piece ? piece : ""} ${selected ? "selected" : ""}`}
       onClick={() => onSelect(id)}
     ></div>
   );
